d94ecb1e8da28e706977e531e2f83e7311085c26
name <giggleinheart@hotmail.com>
2019-11-07 04:02:24 +0700

    edite add sharemenuoption

--- app/src/main/java/buu/example/myapp/MainActivity.kt
+++ app/src/main/java/buu/example/myapp/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return NavigationUI.navigateUp(navController, drawerLayout)
+
     }
 }
 
--- app/src/main/java/buu/example/myapp/detailFragment.kt
+++ app/src/main/java/buu/example/myapp/detailFragment.kt
@@ -33,46 +33,40 @@ class detailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentDetailBinding>(inflater, R.layout.fragment_detail,container,false)
         // Inflate the layout for this fragment
 
-//Toast
-//        Toast.makeText(context,"Share",Toast.LENGTH_LONG).show()
-
+        val binding = DataBindingUtil.inflate<FragmentDetailBinding>(inflater, R.layout.fragment_detail,container,false)
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_detail, container, false)
-
-
-
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.options_menu, menu)
-
+        inflater.inflate(R.menu.share_menu, menu)
         // check if the activity resolves
         if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
             // hide the menu item if it doesn't resolve
-            menu?.findItem(R.id.Share)?.setVisible(false)
+            menu.findItem(R.id.Share)?.setVisible(false)
         }
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item!!.itemId) {
             R.id.Share -> shareSuccess()
         }
-
         return super.onOptionsItemSelected(item)
-
     }
     private fun getShareIntent() : Intent {
 
         val shareIntent = Intent(Intent.ACTION_SEND)
         shareIntent.setType("text/plain")
             .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text))
-
         return shareIntent
     }
     private fun shareSuccess() {
         startActivity(getShareIntent())
     }
+
+
+
 }

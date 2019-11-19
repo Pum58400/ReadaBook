f2556a432842dec7634d2ec45c42e2586db30e4d
name <giggleinheart@hotmail.com>
2019-11-19 08:28:57 +0700

    Explore the fragment lifecycle

--- app/src/main/java/buu/example/myapp/LibraryFragment.kt
+++ app/src/main/java/buu/example/myapp/LibraryFragment.kt
@@ -3,6 +3,7 @@ package buu.example.myapp
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
@@ -32,10 +33,47 @@ class LibraryFragment : Fragment() {
         }
 
         setHasOptionsMenu(true)
+        Log.i("LibraryFragment", "onCreateView called")
         return binding.root
+
     }
 
-
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("LibraryFragment", "onAttach called")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("LibraryFragment", "onCreate called")
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.i("LibraryFragment", "onActivityCreated called")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("LibraryFragment", "onStart called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("LibraryFragment", "onResume called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("LibraryFragment", "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("LibraryFragment", "onStop called")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("LibraryFragment", "onDestroyView called")
+    }
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("LibraryFragment", "onDetach called")
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)

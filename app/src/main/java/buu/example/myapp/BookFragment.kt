861dd9fb43742150d910f79efc45a89153d94524
name <giggleinheart@hotmail.com>
2019-11-21 06:14:32 +0700

    OK im lose

--- app/src/main/java/buu/example/myapp/BookFragment.kt
+++ app/src/main/java/buu/example/myapp/BookFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import buu.example.myapp.databinding.FragmentBookBinding
+import buu.example.myapp.databinding.FragmentDetailBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +20,15 @@ class BookFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book, container, false)
+        val binding = DataBindingUtil.inflate<FragmentBookBinding>(inflater, R.layout.fragment_book,container,false)
+        binding.BACKBtn.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_bookFragment_to_libraryFragment)
+        }
+
+
+
+
+        return binding.root
     }
 
 
